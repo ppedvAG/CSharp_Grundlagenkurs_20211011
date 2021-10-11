1cb59bb6cf5fbd8a2db38d6c1c774d2f66a6851e
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2021-10-11 12:22:43 +0200

    Grundlagenkurs -> Datentypen

---
+++ CSharp_Grundkurs/M001_01_HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace M001_01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write vs. WriteLine
+            // escape-sequences -> https://docs.microsoft.com/de-de/cpp/c-language/escape-sequences?view=msvc-160
+            Console.Write("Hallo Liebe Teilnehmer -> " + Environment.NewLine);
+            Console.Write("Ich hoffe der Kurs wird allen viel Spass bringen \t und Alle lernen viel \n"); //Zeilenumbruch fehlt
+
+            Console.WriteLine("Hallo Liebe Teilnehmer -> ");
+            Console.WriteLine("Ich hoffe der Kurs wird allen viel Spass bringen und Alle lernen viel");
+
+            //Console.ReadLine Sample -> Readline: Eingabe von mehreren Zeichen + [Enter] Taste zum abschließen der Eingabe
+            Console.Write("Wie teuer ist eine Hotelübernachtung > ");
+            string input = Console.ReadLine();
+
+
+
+            //Console.ReadKey
+            Console.Clear(); //Komplette Console wird gelöscht.  We der CLS Befahl in DOS 
+            //cw + [Tab] + [tab]
+            Console.WriteLine("--- Hauptmenü --- ");
+            Console.WriteLine("(1) Menüpunkt 1");
+            Console.WriteLine("(2) Menüpunkt 2");
+            Console.WriteLine("(3) Menüpunkt 3");
+
+            ConsoleKeyInfo inputKey = Console.ReadKey(); //Readkey nimmt das erste eingegebene Charakter und erzeugt keinen Zeilenumbruch
+
+            Console.WriteLine($"\nDeine Menüeingabe ist: {inputKey.KeyChar}" );
+
+
+            if (inputKey.Key == ConsoleKey.NumPad1 || inputKey.Key == ConsoleKey.D1)
+            {
+                Console.WriteLine("Menüpunkt 1 wurde ausgewählt. ");
+            }
+
+        }
+    }
+}
---
+++ CSharp_Grundkurs/M002_01_Datentypen/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace M002_01_Datentypen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C# Datentypen
+
+            //char -> Ein Zeichen
+            char letter = 'A';
+            Console.WriteLine(letter);
+
+            if(char.IsLetter(letter))
+            {
+                Console.WriteLine("Es wurde ein Buchstabe eingegeben");
+            }
+            else if (char.IsNumber(letter))
+            {
+                Console.WriteLine("Es wurde ein Nummer eingegeben");
+            }
+            else if (char.IsDigit(letter))
+            {
+                Console.WriteLine("Es wurde ein 'Decimal digit' eingegeben");
+            }
+
+
+            #region Zeichenketten mit string
+
+            string stringWithEscapes = "Hallo\nLiebe\nTeilnehmer\n";
+
+            string pfad = "C:\\Windows\\Temp";
+            string bessererPfad = @"C:\Windows\Temp\"; // @ - Zeichen wird für das Darstellen von Windowspfaden verwendet. 
+
+            //schlechteste Art eine Variable + statischen Text auszugeben ist.....
+            string output1 = "Verzeichnisausgabe: " + bessererPfad; //Pluszeichen ist bei string-Verkettungen sehr böse
+
+            Console.WriteLine("Verzeichnisausgabe: {0}", bessererPfad); //Ein Erbe aus C -> printf ("statischen Text {0}, {1}, {3}", variable1, variable2, var3);
+
+            Console.WriteLine($"Verzeichnisausgabe: {bessererPfad}");
+
+            Console.WriteLine(bessererPfad);
+            #endregion                     
+
+        }
+    }
+}
